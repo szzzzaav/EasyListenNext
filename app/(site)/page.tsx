272e9c3b05bd6df207e7600bbe630583a4afefb1
szzzzaav <sulaimuzzz@sina.com>
2024-07-09 18:27:32 +0800

    upload

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,9 @@
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
 
+// 不要缓存，保持最新状态
+export const revalidate = 0;
+
 export default function Home() {
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -5,6 +5,11 @@ import { useState } from "react";
 import Input from "./Input";
 import Button from "./Button";
 import Image from "next/image";
+import toast from "react-hot-toast";
+import useUser from "@/hooks/useUser";
+import uniqid from "uniqid";
+import { useRouter } from "next/navigation";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 const UploadModal = () => {
   const Upload = useUploadModal();
@@ -17,6 +22,9 @@ const UploadModal = () => {
       return Upload.onOpen();
     }
   };
+  const { user } = useUser();
+  const router = useRouter();
+  const supabaseClient = useSupabaseClient();
   const [isLoading, setIsLoading] = useState(false);
   const [openPreview, setOpenPreview] = useState(false);
   const [imageUrl, setImageUrl] = useState<string | null>(null);
@@ -30,8 +38,106 @@ const UploadModal = () => {
       image: null,
     },
   });
-  const onSubmit: SubmitHandler<FieldValues> = (values) => {
-    reset();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    try {
+      const chineseRegex = /[\u4e00-\u9fff]/;
+      const imageFile = values.image?.[0];
+      const songFile = values.song?.[0];
+      const lyricFile = values.lyric?.[0];
+      const title = values.title;
+      const author = values.author;
+
+      if (!user) {
+        return toast.error("without user,please log in");
+      }
+
+      if (!imageFile || !songFile || !lyricFile) {
+        return toast.error("File Missing");
+      }
+
+      if (!title || !author) {
+        return toast.error("Song information Missing");
+      }
+
+      if (
+        chineseRegex.test(imageFile.name) ||
+        chineseRegex.test(lyricFile) ||
+        chineseRegex.test(songFile)
+      ) {
+        return toast.error("File name cannot include chinese");
+      }
+
+      setIsLoading(true);
+      const uid = uniqid();
+      const { data: songData, error: songError } = await supabaseClient.storage
+        .from("songs")
+        .upload(`songs-${songFile.name}-${uid}`, songFile, {
+          cacheControl: "3600",
+          // 允许更新
+          upsert: true,
+        });
+
+      if (songError) {
+        setIsLoading(false);
+        return toast.error("Failed song upload");
+      }
+
+      const { data: lyricData, error: lyricError } =
+        await supabaseClient.storage
+          .from("lyrics")
+          .upload(`lyrics-${lyricFile.name}-${uid}`, lyricFile, {
+            cacheControl: "3600",
+            // 允许更新
+            upsert: true,
+          });
+
+      if (lyricError) {
+        console.log(lyricError);
+        setIsLoading(false);
+        return toast.error("Failed lyric upload");
+      }
+
+      const { data: imageData, error: imageError } =
+        await supabaseClient.storage
+          .from("images")
+          .upload(`images-${imageFile.name}-${uid}`, imageFile, {
+            cacheControl: "3600",
+            // 允许更新
+            upsert: true,
+          });
+
+      if (imageError) {
+        setIsLoading(false);
+        return toast.error("Cover lyric upload");
+      }
+
+      const { error: supabaseError } = await supabaseClient
+        .from("songs")
+        .insert({
+          user_id: user.id,
+          title: values.title,
+          author: values.author,
+          image_path: imageData.path,
+          song_path: songData.path,
+          lyric_path: lyricData.path,
+        });
+
+      if (supabaseError) {
+        setIsLoading(false);
+        return toast.error(supabaseError.message);
+      }
+
+      router.refresh();
+      setIsLoading(false);
+      toast.success("song created");
+      reset();
+      Upload.onClose();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
   };
   return (
     <div>
@@ -100,7 +206,7 @@ const UploadModal = () => {
             >
               Preview
             </Button>
-            <Button>Create</Button>
+            <Button type="submit">Create</Button>
           </div>
         </form>
       </Modal>
