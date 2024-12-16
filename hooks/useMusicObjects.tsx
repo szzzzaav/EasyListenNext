a363ec56a3670d5d63d130b04db30613234f820b
szzzzaav <147496545+szzzzaav@users.noreply.github.com>
2024-12-16 22:01:46 +0800

    Update useMusicObjects.tsx

--- hooks/useMusicObjects.tsx
+++ hooks/useMusicObjects.tsx
@@ -11,7 +11,9 @@ interface UseMusicObjectsProps {
   data: any[];
 }
 
-const useMusicObjects = ({ data }: UseMusicObjectsProps) => {
+const useMusicObjects = ({
+  data,
+}: UseMusicObjectsProps) => {
   const {
     dispatch,
     volumnRef,
@@ -30,7 +32,9 @@ const useMusicObjects = ({ data }: UseMusicObjectsProps) => {
     const getData = async () => {
       dispatch({ type: "setPending" });
       if (data.length === 1) {
-        dispatch({ type: "setFirstLoading" });
+        dispatch({
+          type: "setFirstLoading",
+        });
       }
       try {
         let music: any;
@@ -38,7 +42,10 @@ const useMusicObjects = ({ data }: UseMusicObjectsProps) => {
         if (latestData.music) {
           music = latestData.music;
         } else {
-          const { music: audioData } = await getSongById(latestData);
+          const { music: audioData } =
+            await getSongById(
+              latestData
+            );
           music = audioData;
         }
 
@@ -50,89 +57,143 @@ const useMusicObjects = ({ data }: UseMusicObjectsProps) => {
         });
         if (find) return;
 
-        const loadAudio = (Worker: Worker, url: string) => {
+        const loadAudio = (
+          Worker: Worker,
+          url: string
+        ) => {
           return new Promise<{
             success: boolean;
             ArrayBuffer: ArrayBuffer;
             error?: string;
           }>((resolve) => {
-            Worker.onmessage = (event) => resolve(event.data);
+            Worker.onmessage = (
+              event
+            ) => resolve(event.data);
             Worker.postMessage({ url });
           });
         };
 
-        const loadLyric = (Worker: Worker, url: string) => {
+        const loadLyric = (
+          Worker: Worker,
+          url: string
+        ) => {
           return new Promise<{
             success: boolean;
             text: string;
             error?: string;
           }>((resolve) => {
-            Worker.onmessage = (event) => resolve(event.data);
+            Worker.onmessage = (
+              event
+            ) => resolve(event.data);
             Worker.postMessage({ url });
           });
         };
 
         const songWorker = new Worker(
-          new URL("@/workers/loadAudioWorker.ts", import.meta.url)
+          new URL(
+            "@/workers/loadAudioWorker.ts",
+            import.meta.url
+          )
         );
         const lyricWorker = new Worker(
-          new URL("@/workers/loadLyricWorker.ts", import.meta.url)
+          new URL(
+            "@/workers/loadLyricWorker.ts",
+            import.meta.url
+          )
         );
 
         const songUrl = supabase.storage
           .from("songs")
-          .getPublicUrl(music.song_path);
-        const lyricUrl = supabase.storage
-          .from("lyrics")
-          .getPublicUrl(music.lyric_path);
-        const imageUrl = supabase.storage
-          .from("images")
-          .getPublicUrl(music.image_path);
-        music.image_path = imageUrl.data.publicUrl;
+          .getPublicUrl(
+            music.song_path
+          );
+        const lyricUrl =
+          supabase.storage
+            .from("lyrics")
+            .getPublicUrl(
+              music.lyric_path
+            );
+        const imageUrl =
+          supabase.storage
+            .from("images")
+            .getPublicUrl(
+              music.image_path
+            );
+        music.image_path =
+          imageUrl.data.publicUrl;
 
-        const [songResult, lyricResult] = await Promise.all([
-          loadAudio(songWorker, songUrl.data.publicUrl),
-          loadLyric(lyricWorker, lyricUrl.data.publicUrl),
+        const [
+          songResult,
+          lyricResult,
+        ] = await Promise.all([
+          loadAudio(
+            songWorker,
+            songUrl.data.publicUrl
+          ),
+          loadLyric(
+            lyricWorker,
+            lyricUrl.data.publicUrl
+          ),
         ]);
 
-        const newMusicObject = await new AudioControlObject(
-          songResult.ArrayBuffer,
-          {
-            visualStageRef,
-            progressRef,
-            progressBgRef,
-            volumnRef,
-            timeProgressRef,
-          }
-        );
-        const newLyric = new lyricObject({
-          text: lyricResult.text as any,
-          lyricStageRef: lyricStageRef as any,
-          offset: 4,
-          id: music.id as any,
-        });
+        const newMusicObject =
+          await new AudioControlObject(
+            songResult.ArrayBuffer,
+            {
+              visualStageRef,
+              progressRef,
+              progressBgRef,
+              volumnRef,
+              timeProgressRef,
+              lyricStageRef,
+            }
+          );
+        const newLyric =
+          new lyricObject({
+            text: lyricResult.text as any,
+            lyricStageRef:
+              lyricStageRef as any,
+            offset: 4,
+            id: music.id as any,
+          });
 
         setMusicObjects((m) => [
           ...m,
-          { musicObject: newMusicObject, music, lyric: newLyric },
+          {
+            musicObject: newMusicObject,
+            music,
+            lyric: newLyric,
+          },
         ]);
 
         newMusicObject.register({
-          showlryonlrcstage: newLyric.showlryonlrcstage.bind(newLyric),
+          showlryonlrcstage:
+            newLyric.showlryonlrcstage.bind(
+              newLyric
+            ),
         });
 
         if (data.length === 1) {
           dispatch({
             type: "setMusic",
-            payload: { musicObject: newMusicObject, music, lyric: newLyric },
+            payload: {
+              musicObject:
+                newMusicObject,
+              music,
+              lyric: newLyric,
+            },
           });
         }
       } catch (error) {
         alert(error);
       } finally {
-        dispatch({ type: "finishPending" });
+        dispatch({
+          type: "finishPending",
+        });
         if (data.length === 1) {
-          dispatch({ type: "finishFirstLoad" });
+          dispatch({
+            type: "finishFirstLoad",
+          });
         }
       }
     };
